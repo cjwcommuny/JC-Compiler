d869799f9a10bbea18b87a2337239b3e5f6b158d
cjwcommuny <cjwcommuny@outlook.com>
2019-05-01 15:37:47 +0800

    start refactor

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,14 +4,13 @@ import org.antlr.v4.runtime.tree.ParseTreeWalker;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-//        Lexer lexer = new ArrayInitLexer(CharStreams.fromString("{99, 3, 451}"));
         Lexer lexer = new ArrayInitLexer(CharStreams.fromStream(System.in));
         CommonTokenStream tokenStream = new CommonTokenStream(lexer);
         ArrayInitParser parser = new ArrayInitParser(tokenStream);
         ParseTree tree = parser.init();
-//        System.out.println(tree.toStringTree(parser));
+        System.out.println(tree.toStringTree(parser));
         ParseTreeWalker walker = new ParseTreeWalker();
-        walker.walk(new ShortToUnicodeString(), tree);
+//        walker.walk(new ShortToUnicodeString(), tree);
         System.out.println();
     }
 }
