51cc1b560f1a4b376cb804016520b0ddb5b2a531
cjwcommuny <cjwcommuny@outlook.com>
2019-05-13 21:37:06 +0800

    change type of println

--- src/main/java/symbol/InitSymbolImporter.java
+++ src/main/java/symbol/InitSymbolImporter.java
@@ -4,7 +4,6 @@ import ast.node.definition.DefinitionNode;
 import ast.node.definition.DefinitionNodeBuilder;
 import ast.node.definition.FunctionDefinitionNode;
 import ast.node.reference.FunctionNameNode;
-import ast.node.reference.RefNodeBuilder;
 import common.CommonInfrastructure;
 import type.typetype.*;
 
@@ -12,39 +11,39 @@ import java.util.*;
 
 public class InitSymbolImporter {
     private static String stringTypeName = "String";
-    private static String[] stringRestrictNames = new String[]{"java", "lang"};
-    ObjectType stringType;
+    private static List<String> stringRestrictNames = new LinkedList<>(Arrays.asList("java", "lang"));
+    private static ObjectType stringType = TypeBuilder.generateObjectType(stringTypeName, stringRestrictNames);
 
-    private static String printFunctionName = "print";
+    private static String printlnFunctionName = "println";
+    private static Type returnTypeOfPrintln = new VoidType();
+    private static List<Type> parametersTypeOfPrintln = new LinkedList<>(Arrays.asList(stringType, new IntType()));
 
     public void importInitNames(Scope scope) {
         Map<String, DefinitionNode> table = new HashMap<>();
         table.put(stringTypeName, generatePlaceholderNode(stringTypeName, getStringRestrictNames()));
-        table.put(printFunctionName, generatePrintNode());
+        table.put(printlnFunctionName,
+                generateFunctionNode(printlnFunctionName,
+                        returnTypeOfPrintln,
+                        parametersTypeOfPrintln));
         scope.putAll(table);
     }
 
     private DefinitionNode generatePlaceholderNode(String typeName, List<String> restrictNames) {
         ObjectType type = TypeBuilder.generateObjectType(typeName, restrictNames);
-        stringType = type;
         return new DefinitionNode(type, null);
     }
 
     public static List<String> getStringRestrictNames() {
-        return Arrays.asList(stringRestrictNames);
+        return stringRestrictNames;
     }
 
     public static String getStringTypeName() {
         return stringTypeName;
     }
 
-    private DefinitionNode generatePrintNode() {
-        String functionName = "print";
+    private DefinitionNode generateFunctionNode(String functionName, Type returnType, List<Type> parametersType) {
         List<String> restrictNames = new LinkedList<>();
         String fullRestrictName = CommonInfrastructure.constructDefaultFullRestrictName(functionName, restrictNames);
-        Type returnType = new VoidType();
-        List<Type> parametersType = new LinkedList<>();
-        parametersType.add(stringType);
         FunctionType functionType = TypeBuilder.generateFunctionType(parametersType, returnType);
         FunctionNameNode functionNameNode = new FunctionNameNode(functionName, null, functionType);
         FunctionDefinitionNode thisNode = DefinitionNodeBuilder.generateFunctionDefinitionNode(fullRestrictName,
