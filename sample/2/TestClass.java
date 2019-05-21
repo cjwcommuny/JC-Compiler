235a849f6c132498dc75903e167b3d93593001af
cjwcommuny <cjwcommuny@outlook.com>
2019-05-21 19:25:11 +0800

    temp

--- sample/2/TestClass.java
+++ sample/2/TestClass.java
@@ -1,12 +1,7 @@
 public class TestClass {
-    String str = new String();
+    int[] arr = { 1, 2, 3 };
 
     public static void main(String[] args) {
-        InnerTest innerTest = new InnerTest();
-        int t = innerTest.a;
-    }
 
-    public static class InnerTest {
-        public int a = 0;
     }
 }
